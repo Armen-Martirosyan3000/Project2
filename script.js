34f18910920498e5013bfb159ae072fca82a138b
Armen-Martirosyan3000 <Armen11a1@mail.ru>
2022-07-22 14:31:30 +0400

    change1

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+let i = 0; 			// Start Point
+let images = [];	// Images Array
+let images2 = [];
+let time = 1300;	// Time Between Switch
+	 
+// Image List
+
+images[0] = "https://img.freepik.com/premium-photo/excited-young-man-doing-online-shopping-cyber-monday-black-friday-concept-big-sale-shopping-mall-positive-man-enjoying-shopping-quality-service-home-shopping-easy-fast_474717-49988.jpg?w=2000";
+images[1] = "https://thumbs.dreamstime.com/b/man-shopping-bags-26038415.jpg";
+images[2] = "https://st.depositphotos.com/3662505/5113/i/600/depositphotos_51137605-stock-photo-shopping.jpg";
+images2[0] = "https://previews.123rf.com/images/pemotret/pemotret0901/pemotret090100122/4208408-aislados-j%C3%B3venes-caucasian-hombre-llevando-bolsas-de-la-compra.jpg?fj=1";
+images2[1] = "https://previews.123rf.com/images/elwynn/elwynn1406/elwynn140600210/29290021-young-asian-man-shopping-and-holding-bags-full-length-portrait-isolated-on-white-background-.jpg";
+images2[2] = "https://thumbs.dreamstime.com/b/young-asian-man-walking-holding-shopping-bags-full-body-portrait-56386404.jpg";
+
+
+// Change Image
+function changeImg(){
+	
+ 
+
+
+	// Check If Index Is Under Max
+	if(i < images.length){
+	  document.add1.src = images[i];
+    document.add4.src = images2[i];
+    // Add 1 to Index
+	  i++;
+    
+	} 
+  
+  else  { 
+		// Reset Back To O
+		i = 0;
+
+	}
+
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // animation-անիմացիա
 const colors = ["#3CC157", "#2AA7FF", "#BDFF16", "#FCBC0F", "#FF75FD","#FF0077","#3A00FF"];
 
@@ -39,3 +104,7 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+
+
+
